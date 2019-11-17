1f3dee50bb2701515128c31de2e31f6391d775de
Malek Haj-Hussein <malekadair@gmail.com>
2019-11-17 14:43:26 -0600

    added accordion and accordion test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
+// import React, { Component } from "react";
+// import "./App.css";
+// import Messages from "./Messages";
+// import Tabs from "./state/Tabs.js";
+
+// const tabsProp = [
+//   {
+//     name: "First tab",
+//     content:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque."
+//   },
+//   {
+//     name: "Second tab",
+//     content:
+//       "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit."
+//   },
+//   {
+//     name: "Third tab",
+//     content:
+//       "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur."
+//   }
+// ];
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Tabs tabs={tabsProp} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 import React, { Component } from "react";
 import "./App.css";
-import Messages from "./Messages";
-import Tabs from "./state/Tabs.js";
 
-const tabsProp = [
-  {
-    name: "First tab",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque."
-  },
-  {
-    name: "Second tab",
-    content:
-      "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit."
-  },
-  {
-    name: "Third tab",
-    content:
-      "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur."
-  }
-];
+import DemonynApp from "./demonymapp/demonymApp";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Tabs tabs={tabsProp} />
+        <DemonynApp />
       </div>
     );
   }
--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,1 +1,46 @@
-import React
+import React from "react";
+
+class Accordion extends React.Component {
+  static defaultProps = {
+    sections: []
+  };
+
+  state = {
+    currentIndex: null
+  };
+
+  handleButtonClick(index) {
+    this.setState({ currentIndex: index });
+  }
+
+  renderSections() {
+    return this.props.sections.map((section, index) => {
+      return (
+        <div>
+          <li key={index}>
+            <button onClick={() => this.handleButtonClick(index)}>
+              {section.title}
+            </button>
+            {this.renderContent(section, index)}
+          </li>
+        </div>
+      );
+    });
+  }
+
+  renderContent(section, index) {
+    if (this.state.currentIndex === index) {
+      return <p>{section.content}</p>;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderSections()}</ul>
+      </div>
+    );
+  }
+}
+
+export default Accordion;
